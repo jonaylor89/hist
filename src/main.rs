ca309b0c4e1485550e955d5f5f9d941ae0c7ceec
Johannes Naylor <jonaylor89@gmail.com>
2021-11-12 12:50:49 -0500

    update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
-
-extern crate v_hist;
 extern crate rand;
+extern crate v_hist;
 
 use rand::Rng;
 
 fn main() {
     let mut h = v_hist::init();
-    let mut rng = rand::thread_rng();    
+    let mut rng = rand::thread_rng();
 
     // h.max_width = 50;
 
     for x in 0..200 {
-        h.add_entry(format!("{}", x), rng.gen_range(0, 3000)) ;
+        h.add_entry(format!("{}", x), rng.gen_range(0..3000));
     }
 
     h.draw();
